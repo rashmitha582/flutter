f6b44d6a7f629e22989704fff2f5fc5cb4f92114
rashmitha582 <mrashmi2404@gmail.com>
2025-08-04 14:54:17 +0530

    Add files via upload

---
+++ pandu.c
@@ -0,0 +1,12 @@
+#include<stdo.h>
+#include<conio.h>
+int main()
+{
+char str[15]={'H','e','l','l','o',',','w','o','r','l','d'};
+int i;
+printf("\n the actual string is %s",str);
+printf("\n the string after xor each charecter with o is");
+for(i=o;i<11;i++)
+printf("%c",str[i]^o);
+return0;
+}
